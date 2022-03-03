d9be086807024bd83372b60845f69dbe09f84128
TorakMagno <valentino.zampieri88@gmail.com>
2022-03-03 13:52:16 +0100

    practicaSymfony
    Co-authored-by: Alvaro Lozano <alvarolozano2000@gmail.com>

---
+++ migrations/Version20220302175533.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220302175533 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE articulo (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, titulo CLOB NOT NULL, fecha DATE NOT NULL, texto CLOB NOT NULL, comentario CLOB NOT NULL, resumen CLOB NOT NULL, categoria CLOB NOT NULL, url CLOB NOT NULL, medio CLOB NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE articulo');
+    }
+}
---
+++ src/Controller/ArticuloController.php
@@ -0,0 +1,79 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use App\Entity\Articulo; 
+
+
+class ArticuloController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function paginaInicio(ManagerRegistry $doctrine): Response {
+        $repository = $doctrine->getRepository(Articulo::class);
+        $articulos = $repository->findAll();
+
+
+        return $this->render('home.html.twig', [
+            "articulos" => $articulos
+        ]);
+    }
+
+
+    /**
+     * @Route("/ver/{numero}")
+     */
+    public function inforArticulo(ManagerRegistry $doctrine, $numero) {
+
+        $repository = $doctrine->getRepository(Articulo::class);
+        $articulo = $repository->findOneBy([
+            "id" => $numero
+        ]);
+
+        return $this->render('ver.html.twig', [
+            "articulo" => $articulo            
+        ]);
+    }
+
+
+    /**
+     * @Route("crear")
+     */
+    public function new(Request $request): Response
+    {
+        // creates a task object and initializes some data for this example
+        $articulo = new Articulo();
+        
+
+        $form = $this->createFormBuilder($articulo)
+            ->add('id', NumberType::class)
+            ->add('titulo', TextType::class)
+            ->add('fecha', DateType::class)
+            ->add('texto', TextType::class)
+            ->add('comentario', TextType::class)
+            ->add('resumen', TextType::class)
+            ->add('categoria', TextType::class)
+            ->add('url', TextType::class)
+            ->add('medio', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+
+            return $this->renderForm('form.html.twig', [
+                'form' => $form,
+            ]);
+    }
+
+}
